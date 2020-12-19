0afad5cd36eae7201350729bc5447dbe1ad93470
Supatat Singkomfu <Supatat Singkomfu>
2020-12-19 21:09:44 +0700

    Add Store in components

---
+++ levitate/src/components/Store/StoreElements.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const ListWrapper = styled.div`
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+`
+
+export const List = styled.div`
+    width: 30vw;
+    height: 20vh;
+    background-color: red;
+`
---
+++ levitate/src/components/Store/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { ListWrapper, List } from './StoreElements'
+
+const Store = () => {
+    return (
+        <>
+            <ListWrapper>
+                <List>
+
+                </List>
+            </ListWrapper>
+        </>
+    )
+}
+
+export default Store
