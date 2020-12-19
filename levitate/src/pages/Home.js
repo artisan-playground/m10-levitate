21a173a965f21755913f9d5856ac59ba80c1f4bc
Supatat Singkomfu <Supatat Singkomfu>
2020-12-19 20:19:10 +0700

    Edit Home.js in pages

--- levitate/src/pages/Home.js
+++ levitate/src/pages/Home.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import styled from 'styled-components'
+
+const Title = styled.div`
+  height: 80vh;
+  /* padding-top: 5vh; */
+  /* img {
+    width: 100%;
+  } */
+`
+
+const Wrapper = styled.div`
+  padding: 2% 10%;
+  /* z-index: 1000; */
+  /* img {
+    position: absolute;
+    z-index: -1;
+  } */
+`
 
 const Home = () => {
     return (
         <>
             <Navbar />
+            <Title />
+            <Wrapper />
         </>
     )
 }
