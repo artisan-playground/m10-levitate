6570b52b754abe1cbe72a103138bac7f22f95646
Supatat Singkomfu <Supatat Singkomfu>
2020-12-19 15:59:31 +0700

    Edit App

--- levitate/src/App.js
+++ levitate/src/App.js
@@ -1,12 +1,47 @@
-import React from 'react'
-import ParkingContext from './contexts/ParkingContext'
+import React, { Suspense } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import styled from 'styled-components'
+// import Footer from './components/Footer'
+import GlobalStyle from './components/GlobalStyle'
+import NavBar from './components/Navbar'
+// import TempValueContext from './contexts/TempValueContext'
+import Home from './pages/Home'
+import Parking from './pages/Parking'
+// import Connection from './services/Connection'
+import Navbar from './components/Navbar'
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`
+
+const ContentWrap = styled.div`
+  flex: 1;
+`
 
 function App() {
   return (
     <>
-      <ParkingContext>
-        
-      </ParkingContext>
+      {/* <TempValueContext> */}
+      {/* <Connection /> */}
+      <PageContainer>
+        <Navbar />
+        <ContentWrap>
+          <GlobalStyle />
+          <Router>
+            <NavBar />
+            <Suspense fallback="...Loading please wait...">
+              <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/Parking" exact component={Parking} />
+              </Switch>
+            </Suspense>
+          </Router>
+        </ContentWrap>
+        {/* <Footer /> */}
+      </PageContainer>
+      {/* </TempValueContext> */}
     </>
   )
 }
