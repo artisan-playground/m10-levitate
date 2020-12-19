58e76dbf1aba00d1ee0e94106da2c7d396c45857
Supatat Singkomfu <Supatat Singkomfu>
2020-12-20 00:22:20 +0700

    Add Application in components

---
+++ levitate/src/components/Application/ApplicationElements.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Box = styled.div`
+    height: 70vh;
+    display: flex;
+    flex-direction: row;
+    background-color: #525273;
+    border-radius: 30px;
+`
