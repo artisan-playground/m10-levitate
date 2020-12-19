93ea83c1576eaab1575e463d064a5908ea1979c9
Supatat Singkomfu <Supatat Singkomfu>
2020-12-20 00:23:33 +0700

    Add Recommended in components

---
+++ levitate/src/components/Recommended/RecommendedElements.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const ContainerRec = styled.div`
+    display: flex;
+    flex-direction: row;
+`
+
+export const ImagesRec = styled.div`
+    width: 50%;
+    img {
+        width: 100%;
+    }
+`
+
+export const ImagesDetails = styled.div`
+    width: 50%;
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 0 0 50px;
+    p {
+    text-indent: 50px;
+    text-transform: uppercase;
+    }
+`
