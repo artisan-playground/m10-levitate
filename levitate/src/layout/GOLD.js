b54af912bf3760fc2e03b7964dcd80360ac56087
sahatsawat1 <sahatsawatfriend@gmail.com>
2020-12-20 01:11:30 +0700

    Create GOLD layout

---
+++ levitate/src/layout/GOLD.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const BoxWrapper = styled.div`
+    background-color: rgba(227, 187, 28, 0.5);
+    border: 8px solid rgba(227, 187, 28, 1);
+    border-bottom: 0;
+    width: 2.5%;
+    height: 46px;
+    margin: 8px;
+    h1{
+        color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 46px;
+    }
+`
+
+const BoxParking = ({NO}) => {
+    return (
+        <>
+            <BoxWrapper>
+                <h1>{NO}</h1>
+            </BoxWrapper>
+        </>
+    )
+}
+
+export default BoxParking
