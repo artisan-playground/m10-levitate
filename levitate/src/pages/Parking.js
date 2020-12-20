072d2bdc9c245acf7ad4e12c7ec7dcc9b3521c5b
sahatsawat1 <sahatsawatfriend@gmail.com>
2020-12-20 08:52:05 +0700

    Edit Parking

--- levitate/src/pages/Parking.js
+++ levitate/src/pages/Parking.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
 import WHITE from '../layout/WHITE'
+import WHITERow from '../layout/WHITERow'
 import PICK from '../layout/PICK'
+import PICKRow from '../layout/PICKRow'
 import BLUE from '../layout/BLUE'
+import BLUERow from '../layout/BLUERow'
 import GOLD from '../layout/GOLD'
+import GOLDRow from '../layout/GOLDRow'
+import CAR from '../layout/CAR'
+import CARRow from '../layout/CARRow'
+import Status from '../assets/Group 100.png'
 
 const GhostNavbar = styled.div`
   height: 64px;
@@ -12,73 +19,109 @@ const GhostNavbar = styled.div`
 
 const LOT = styled.div`
     display: flex;
+    padding: 0.5%;
+    width: 99%;
 `
 
 const LOTWrapper = styled.div`
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    height: 55vh;
+    border-radius: 14px;
+    padding-top: 10%;
+    position: relative;
+`
+
+const Wrapper = styled.div`
+    background-color: rgba(82,95,115,1);
+    padding: 3%;
+    border-radius: 16px;
+`
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+const Image = styled.img`
+    position: absolute;
+    top: 85px;
+    left: -2%;
+    width: 14%;
 
 `
 
 const Parking = () => {
     return (
-        <>
+        <Container>
+
             <GhostNavbar />
 
             <LOTWrapper>
 
-                <LOT>
-                    <GOLD NO='A1' />
-                    <GOLD NO='A2' />
-                    <GOLD NO='A3' />
-                    <WHITE NO='A4' />
-                    <WHITE NO='A5' />
-                    <WHITE NO='A6' />
-                    <WHITE NO='A7' />
-                    <WHITE NO='A8' />
-                    <WHITE NO='A9' />
-                    <WHITE NO='A10' />
-                </LOT>
+                <Image src={Status} />
 
-                <LOT>
-                    <BLUE NO='B1' />
-                    <GOLD NO='B2' />
-                    <GOLD NO='B3' />
-                    <WHITE NO='B4' />
-                    <WHITE NO='B5' />
-                    <WHITE NO='B6' />
-                    <WHITE NO='B7' />
-                    <WHITE NO='B8' />
-                    <WHITE NO='B9' />
-                    <WHITE NO='B10' />
-                </LOT>
+                <Wrapper>
 
-                <LOT>
-                    <BLUE NO='C1' />
-                    <PICK NO='C2' />
-                    <PICK NO='C3' />
-                    <WHITE NO='C4' />
-                    <WHITE NO='C5' />
-                    <WHITE NO='C6' />
-                    <WHITE NO='C7' />
-                    <WHITE NO='C8' />
-                    <WHITE NO='C9' />
-                    <WHITE NO='C10' />
-                </LOT>
+                    <LOT>
+                        <GOLD NO='A1' />
+                        <GOLD NO='A2' />
+                        <GOLD NO='A3' />
+                        <WHITE NO='A4' />
+                        <CARRow />
+                        <CARRow />
+                        <CARRow />
+                        <WHITE NO='A8' />
+                        <WHITE NO='A9' />
+                        <WHITE NO='A10' />
+                    </LOT>
 
-                <LOT>
-                    <PICK NO='D1' />
-                    <PICK NO='D2' />
-                    <PICK NO='D3' />
-                    <WHITE NO='D4' />
-                    <WHITE NO='D5' />
-                    <WHITE NO='D6' />
-                    <WHITE NO='D7' />
-                    <WHITE NO='D8' />
-                    <WHITE NO='D9' />
-                    <WHITE NO='D10' />
-                </LOT>
+                    <LOT>
+                        <BLUERow NO='B1' />
+                        <GOLDRow NO='B2' />
+                        <GOLDRow NO='B3' />
+                        <WHITERow NO='B4' />
+                        <WHITERow NO='B5' />
+                        <WHITERow NO='B6' />
+                        <CAR />
+                        <WHITERow NO='B8' />
+                        <WHITERow NO='B9' />
+                        <WHITERow NO='B10' />
+                    </LOT>
+
+                    <LOT>
+                        <BLUE NO='C1' />
+                        <PICK NO='C2' />
+                        <PICK NO='C3' />
+                        <WHITE NO='C4' />
+                        <WHITE NO='C5' />
+                        <CARRow />
+                        <WHITE NO='C7' />
+                        <WHITE NO='C8' />
+                        <WHITE NO='C9' />
+                        <WHITE NO='C10' />
+                    </LOT>
+
+                    <LOT>
+                        <PICKRow NO='D1' />
+                        <PICKRow NO='D2' />
+                        <PICKRow NO='D3' />
+                        <WHITERow NO='D4' />
+                        <WHITERow NO='D5' />
+                        <WHITERow NO='D6' />
+                        <WHITERow NO='D7' />
+                        <WHITERow NO='D8' />
+                        <CAR />
+                        <WHITERow NO='D10' />
+                    </LOT>
+
+                </Wrapper>
 
             </LOTWrapper>
-        </>
+
+        </Container>
     )
 }
 
